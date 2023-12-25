384b72a958d807242f53c7625f56f666b97012a9
zzaizzai <junsai0530@gmail.com>
2023-12-26 00:08:46 +0900

    [products] search page, detail page

--- app/products/endpoints.py
+++ app/products/endpoints.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Request
-from db import db_manager, fetch_all_as_dict
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
+from db import db_manager, fetch_all_as_dict
+from .model import ProductControl
 router = APIRouter(prefix="/products")
 
 templates = Jinja2Templates(directory=["app/core/templates", "app/products/templates"])
@@ -10,6 +12,21 @@ templates = Jinja2Templates(directory=["app/core/templates", "app/products/templ
 @router.get("/index")
 async def products_index_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/detail/{product_id}")
+async def product_detail(request: Request, product_id: int):
+    context = {}
+    context['request'] = request
+    
+    product_control = ProductControl(product_id=product_id)
+    product_control.find_a_product_with_id()
+
+    if product_control.exist_product() is False:
+        return RedirectResponse("/products/index")
+
+    context['product'] = product_control.get_a_product()
+    return templates.TemplateResponse("product_model_detail.html", context)
 
 @router.get("/search")
 async def search_products_model(request: Request, q: str = None):
@@ -23,27 +40,14 @@ async def search_products_model(request: Request, q: str = None):
         context['q'] = ''
         return templates.TemplateResponse(html, context)
     
-    cursor = db_manager.get_cursor()
-    cursor.execute(f"""
-        SELECT 
-            *
-        FROM 
-            products_model
-        WHERE 
-            products_model.name LIKE '%{q}%'
-        """)
-        
+    pc = ProductControl(q=q)
+    pc.search_products_with_q()
     
-    products = fetch_all_as_dict(cursor)
-    print(products)
-    cursor.close()
-
-    
-    if len(products) == 0 :
+    if pc.exist_product() is False :
         context['msg'] = "No result"
         return templates.TemplateResponse(html, context)
 
-    context['products'] = products
+    context['products'] = pc.get_products()
     
     return templates.TemplateResponse(html, context)
 
